6471900739645f79fc28b69c344febd92087e412
Ankit Thakur <tankit541@gmail.com>
2019-10-03 16:32:02 +0530

    Update ESTransportClient file

--- src/main/java/com/spring/elasticsearchpractice/configuration/ESTransportClient.java
+++ src/main/java/com/spring/elasticsearchpractice/configuration/ESTransportClient.java
@@ -8,11 +8,22 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * The type Es transport client.
+ */
 @Configuration
 public class ESTransportClient {
 
+    /**
+     * The Logger.
+     */
     Logger logger = LoggerFactory.getLogger(ESTransportClient.class);
 
+    /**
+     * Gets client.
+     *
+     * @return the client
+     */
     @Bean(name = "restHighLevelClient", destroyMethod = "close")
     public RestHighLevelClient getClient() {
         RestHighLevelClient client = new RestHighLevelClient(
