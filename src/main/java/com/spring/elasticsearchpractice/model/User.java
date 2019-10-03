20fa7978936e56b9c51d7f8f06ed12e3c4ab159b
Ankit Thakur <tankit541@gmail.com>
2019-10-03 16:33:30 +0530

    Update User file

--- src/main/java/com/spring/elasticsearchpractice/model/User.java
+++ src/main/java/com/spring/elasticsearchpractice/model/User.java
@@ -2,6 +2,9 @@ package com.spring.elasticsearchpractice.model;
 
 import org.springframework.stereotype.Component;
 
+/**
+ * The type User.
+ */
 @Component
 public class User {
 
@@ -11,46 +14,101 @@ public class User {
     private String mobile;
     private String others;
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Sets id.
+     *
+     * @param id the id
+     * @return the id
+     */
     public User setId(String id) {
         this.id = id;
         return this;
     }
 
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets name.
+     *
+     * @param name the name
+     * @return the name
+     */
     public User setName(String name) {
         this.name = name;
         return this;
     }
 
+    /**
+     * Gets email.
+     *
+     * @return the email
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Sets email.
+     *
+     * @param email the email
+     * @return the email
+     */
     public User setEmail(String email) {
         this.email = email;
         return this;
     }
 
+    /**
+     * Gets mobile.
+     *
+     * @return the mobile
+     */
     public String getMobile() {
         return mobile;
     }
 
+    /**
+     * Sets mobile.
+     *
+     * @param mobile the mobile
+     * @return the mobile
+     */
     public User setMobile(String mobile) {
         this.mobile = mobile;
         return this;
     }
 
+    /**
+     * Gets others.
+     *
+     * @return the others
+     */
     public String getOthers() {
         return others;
     }
 
+    /**
+     * Sets others.
+     *
+     * @param others the others
+     * @return the others
+     */
     public User setOthers(String others) {
         this.others = others;
         return this;
