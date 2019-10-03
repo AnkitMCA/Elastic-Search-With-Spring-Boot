707c13fa807f3bd843a4cf3c7554ac72b039c5d9
Ankit Thakur <tankit541@gmail.com>
2019-10-03 16:32:37 +0530

    Update APIController file

--- src/main/java/com/spring/elasticsearchpractice/controller/APIController.java
+++ src/main/java/com/spring/elasticsearchpractice/controller/APIController.java
@@ -11,6 +11,8 @@ import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.core.TermVectorsRequest;
+import org.elasticsearch.client.core.TermVectorsResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,13 +21,25 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * The type Api controller.
+ */
 @RestController
 @RequestMapping("api")
 public class APIController {
 
+    /**
+     * The Rest high level client.
+     */
     @Autowired
     RestHighLevelClient restHighLevelClient;
 
+    /**
+     * Index response entity.
+     *
+     * @param user the user
+     * @return the response entity
+     */
     @PostMapping("index")
     public ResponseEntity index(@RequestBody User user) {
         Map<String, Object> json = new HashMap<>();
@@ -47,6 +61,33 @@ public class APIController {
         return ResponseEntity.ok(indexResponse);
     }
 
+    /**
+     * Exists api response entity.
+     *
+     * @param value the value
+     * @return the response entity
+     */
+    @GetMapping("exists/{id}")
+    public ResponseEntity existsAPI(@PathVariable("id") String value) {
+        GetRequest getRequest = new GetRequest("myindex")
+                .id(value);
+        Boolean exists = false;
+        try {
+            exists = restHighLevelClient.exists(getRequest, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            System.out.println("There is an Exception in exists method.");
+            e.printStackTrace();
+        }
+
+        return ResponseEntity.ok(exists);
+    }
+
+    /**
+     * Gets api.
+     *
+     * @param value the value
+     * @return the api
+     */
     @GetMapping("get/{id}")
     public ResponseEntity getAPI(@PathVariable("id") String value) {
         GetRequest getRequest = new GetRequest("myindex")
@@ -62,6 +103,13 @@ public class APIController {
         return ResponseEntity.ok(getResponse);
     }
 
+    /**
+     * Update api response entity.
+     *
+     * @param user  the user
+     * @param value the value
+     * @return the response entity
+     */
     @PostMapping("update/{id}")
     public ResponseEntity updateAPI(@RequestBody User user, @PathVariable("id") String value) {
         Map<String, Object> json = new HashMap<>();
@@ -84,6 +132,12 @@ public class APIController {
         return ResponseEntity.ok(updateResponse);
     }
 
+    /**
+     * Delete api response entity.
+     *
+     * @param value the value
+     * @return the response entity
+     */
     @GetMapping("delete/{id}")
     public ResponseEntity deleteAPI(@PathVariable("id") String value) {
         DeleteRequest deleteRequest = new DeleteRequest("myindex", value);
@@ -96,5 +150,39 @@ public class APIController {
         }
 
         return ResponseEntity.ok(deleteResponse);
+    }
+
+    /**
+     * Term vectors api response entity.
+     *
+     * @return the response entity
+     */
+    @GetMapping("termvectors")
+    public ResponseEntity termVectorsAPI() {
+
+        //        This below code is for generating term vectors for the documents not present in the index.
+
+        /*XContentBuilder docBuilder = null;
+        try {
+            docBuilder = XContentFactory.jsonBuilder();
+            docBuilder.startObject().field("name", "Another Name").endObject();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        TermVectorsRequest termVectorsRequest = new TermVectorsRequest("myindex", docBuilder);
+        */
+
+        TermVectorsRequest termVectorsRequest = new TermVectorsRequest("myindex", "2");
+        termVectorsRequest.setFields("name");
+
+        TermVectorsResponse termVectorsResponse = null;
+        try {
+            termVectorsResponse = restHighLevelClient.termvectors(termVectorsRequest, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            System.out.println("There is an Exception in term vectors method.");
+            e.printStackTrace();
+        }
+
+        return ResponseEntity.ok(termVectorsResponse);
     }
 }
