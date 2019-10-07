8550378c4ec4fd7af53d4fdb2bcebec53672c6fe
Ankit Thakur <tankit541@gmail.com>
2019-10-07 15:32:25 +0530

    Update IndexAPIsController file

--- src/main/java/com/spring/elasticsearchpractice/controller/IndexAPIsController.java
+++ src/main/java/com/spring/elasticsearchpractice/controller/IndexAPIsController.java
@@ -1,9 +1,10 @@
 package com.spring.elasticsearchpractice.controller;
 
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
-import org.elasticsearch.client.indices.AnalyzeRequest;
-import org.elasticsearch.client.indices.AnalyzeResponse;
+import org.elasticsearch.client.indices.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,6 +26,10 @@ public class IndexAPIsController {
     @Autowired
     RestHighLevelClient restHighLevelClient;
 
+    /*
+    Analyze Request is used to analyze the word or the sentence using default as well as custom analyzer.
+    */
+
     /**
      * Analyze api response entity.
      *
@@ -41,6 +46,68 @@ public class IndexAPIsController {
             return ResponseEntity.ok(analyzeResponse);
         } catch (IOException e) {
             System.out.println("There is an Exception in analyze method.");
+            e.printStackTrace();
+        }
+
+        return (ResponseEntity) ResponseEntity.badRequest();
+    }
+
+    /**
+     * Create index api response entity.
+     *
+     * @return the response entity
+     */
+    @GetMapping("createindex")
+    public ResponseEntity createIndexAPI() {
+        CreateIndexRequest createIndexRequest = new CreateIndexRequest("demoindex");
+        /*createIndexRequest.settings(Settings.builder()
+                .put("index.number_of_shards", 3)
+                .put("index.number_of_replicas", 2)
+        );*/
+        CreateIndexResponse createIndexResponse = null;
+        try {
+            createIndexResponse = restHighLevelClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
+            return ResponseEntity.ok(createIndexResponse);
+        } catch (IOException e) {
+            System.out.println("There is an Exception in createIndex method.");
+            e.printStackTrace();
+        }
+
+        return (ResponseEntity) ResponseEntity.badRequest();
+    }
+
+    /**
+     * Delete index api response entity.
+     *
+     * @return the response entity
+     */
+    @GetMapping("deleteindex")
+    public ResponseEntity deleteIndexAPI() {
+        DeleteIndexRequest deleteIndexRequest = new DeleteIndexRequest("demoindex");
+        AcknowledgedResponse deleteIndexResponse = null;
+        try {
+            deleteIndexResponse = restHighLevelClient.indices().delete(deleteIndexRequest, RequestOptions.DEFAULT);
+            return ResponseEntity.ok(deleteIndexResponse);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return (ResponseEntity) ResponseEntity.badRequest();
+    }
+
+    /**
+     * Index exists api response entity.
+     *
+     * @return the response entity
+     */
+    @GetMapping("indexexists")
+    public ResponseEntity indexExistsAPI() {
+        GetIndexRequest getIndexRequest = new GetIndexRequest("demoindex");
+        try {
+            boolean response = restHighLevelClient.indices().exists(getIndexRequest, RequestOptions.DEFAULT);
+            return ResponseEntity.ok(response);
+        } catch (IOException e) {
+            System.out.println("There is an Exception in indexExists method.");
             e.printStackTrace();
         }
 
